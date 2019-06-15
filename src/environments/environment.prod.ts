92f1bc138608e6646c1dddbe99d50e9b2de5492f
chelolm <chelolm@uoc.edu>
2019-06-15 20:32:58 +0200

    Ejercicio 6 - Producción es puerto 80, se lanza con http-server -p 80 -c-1 dist/browser / QA es puerto 8080, se lanza con http-server -p 8080 -c-1 dist/browser

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_ENDPOINT: "http://172.0.0.1/api/" // ENDPOINT a nuestra IP de producción (simulamos con 172.0.0.1 y puerto 80)
 };
---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  API_ENDPOINT: "http://172.0.0.1:8080/qa-api/" // ENDPOINT a nuestra IP de QA (simulamos con 172.0.0.1 y puerto 8080)
+};
