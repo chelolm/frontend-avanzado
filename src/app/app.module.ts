affe6ff52f7e5c06807041fa56508ae544250cf5
chelolm <chelolm@uoc.edu>
2019-06-15 22:44:29 +0200

    Ejercicio 7 - Preload modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './shared/core.module';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { rootRouterConfig } from './app-routing';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -27,7 +27,7 @@ export const NGRX_STATE = makeStateKey('NGRX_STATE');
   imports: [
     SharedModule,
     CoreModule,
-    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
+    RouterModule.forRoot(rootRouterConfig, { useHash: false, preloadingStrategy: PreloadAllModules }),
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(FakeBackendService, {
       dataEncapsulation: false
